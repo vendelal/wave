b77a39a5f25ecec1b643f3bbdc907796fd32e7c5
vendelal <vendelalinnea@mac.com>
2015-12-15 16:02:54 -0500

    Updated Screen

--- Wave/ViewController.swift
+++ Wave/ViewController.swift
@@ -19,31 +19,126 @@ class ViewController: UIViewController {
     @IBOutlet weak var blueButton: UIButton!
     @IBOutlet weak var frequencySlider: UISlider!
     @IBOutlet weak var colorText: UILabel!
+    @IBOutlet weak var controls: UIImageView!
+    @IBOutlet weak var whiteSelected: UIImageView!
+    @IBOutlet weak var redSelected: UIImageView!
+    @IBOutlet weak var yellowSelected: UIImageView!
+    @IBOutlet weak var greenSelected: UIImageView!
+    @IBOutlet weak var blueSelected: UIImageView!
+    @IBOutlet weak var instructions: UIImageView!
+    @IBOutlet weak var muteButton: UIButton!
+    @IBOutlet weak var soundButton: UIButton!
     
-    @IBAction func openButton(sender: AnyObject) {
-        whiteButton.hidden = false
-        redButton.hidden = false
-        yellowButton.hidden = false
-        greenButton.hidden = false
-        blueButton.hidden = false
-        frequencySlider.hidden = false
-        label.hidden = false
-        colorText.hidden = false
-        isOpen = true
+    @IBAction func muteButton(sender: AnyObject) {
+        muteButton.hidden = true
+        soundButton.hidden = false
     }
-    @IBAction func closeButton(sender: AnyObject) {
-        whiteButton.hidden = true
-        redButton.hidden = true
-        yellowButton.hidden = true
-        greenButton.hidden = true
-        blueButton.hidden = true
+    
+    @IBAction func soundButton(sender: AnyObject) {
+        soundButton.hidden = true
+        muteButton.hidden = false
+    }
+    
+    @IBAction func helpButton(sender: AnyObject) {
+        controls.hidden = true
         frequencySlider.hidden = true
-        label.hidden = true
-        colorText.hidden = true
-        isOpen = true
-
+        whiteButton.hidden = true
+        whiteSelected.hidden = true
+        redButton.hidden = true
+        redSelected.hidden = true
+        yellowButton.hidden = true
+        yellowSelected.hidden = true
+        greenButton.hidden = true
+        greenSelected.hidden = true
+        blueButton.hidden = true
+        blueSelected.hidden = true
+        
+        if (instructions.hidden == true) {
+            instructions.hidden = false
+        }
+        else {
+            instructions.hidden = true
+        }
     }
     
+    @IBAction func lightButton(sender: AnyObject) {
+        instructions.hidden = true
+        if (whiteButton.hidden == true) {
+            whiteButton.hidden = false
+        }
+        else {
+            whiteButton.hidden = true
+        }
+        
+        if (redButton.hidden == true) {
+            redButton.hidden = false
+        }
+        else {
+            redButton.hidden = true
+        }
+        
+        if (yellowButton.hidden == true) {
+            yellowButton.hidden = false
+        }
+        else {
+            yellowButton.hidden = true
+        }
+        
+        if (greenButton.hidden == true) {
+            greenButton.hidden = false
+        }
+        else {
+            greenButton.hidden = true
+        }
+        
+        if (blueButton.hidden == true) {
+            blueButton.hidden = false
+        }
+        else {
+            blueButton.hidden = true
+        }
+        
+        if (frequencySlider.hidden == true) {
+            frequencySlider.hidden = false
+        }
+        else {
+            frequencySlider.hidden = true
+        }
+        
+        if (label.hidden == true) {
+            label.hidden = false
+        }
+        else {
+            label.hidden = true
+        }
+        
+        if (controls.hidden == true) {
+            controls.hidden = false
+        }
+        else {
+            controls.hidden = true
+        }
+        
+        if (whiteSelected.hidden == false) {
+            whiteSelected.hidden = true
+        }
+        
+        if (redSelected.hidden == false) {
+            redSelected.hidden = true
+        }
+        
+        if (yellowSelected.hidden == false) {
+            yellowSelected.hidden = true
+        }
+        
+        if (greenSelected.hidden == false) {
+            greenSelected.hidden = true
+        }
+        
+        if (blueSelected.hidden == false) {
+            blueSelected.hidden = true
+        }
+    }
     
     @IBAction func redButton(sender: AnyObject) {
         clickedColor = "(255,0,0)"
@@ -71,18 +166,43 @@ class ViewController: UIViewController {
     func checkColor() {
         if (clickedColor == "(255,0,0)") {
             colorText.textColor  = UIColor.redColor()
+            redSelected.hidden = false
+            whiteSelected.hidden = true
+            yellowSelected.hidden = true
+            greenSelected.hidden = true
+            blueSelected.hidden = true
         }
         if (clickedColor == "(0,255,0)") {
             colorText.textColor  = UIColor.greenColor()
+            greenSelected.hidden = false
+            redSelected.hidden = true
+            whiteSelected.hidden = true
+            yellowSelected.hidden = true
+            blueSelected.hidden = true
         }
         if (clickedColor == "(0,0,255)") {
             colorText.textColor  = UIColor.blueColor()
+            blueSelected.hidden = false
+            greenSelected.hidden = true
+            redSelected.hidden = true
+            whiteSelected.hidden = true
+            yellowSelected.hidden = true
         }
         if (clickedColor == "(255,255,0)") {
             colorText.textColor  = UIColor.yellowColor()
+            yellowSelected.hidden = false
+            greenSelected.hidden = true
+            redSelected.hidden = true
+            whiteSelected.hidden = true
+            blueSelected.hidden = true
         }
         if (clickedColor == "(255,255,255)") {
             colorText.textColor = UIColor.whiteColor()
+            whiteSelected.hidden = false
+            redSelected.hidden = true
+            yellowSelected.hidden = true
+            greenSelected.hidden = true
+            blueSelected.hidden = true
         }
     }
     
@@ -116,6 +236,20 @@ class ViewController: UIViewController {
         AKOrchestra.addInstrument(instrument)
         AKOrchestra.addInstrument(fm)
         
+        whiteButton.hidden = true
+        whiteSelected.hidden = true
+        redButton.hidden = true
+        redSelected.hidden = true
+        yellowButton.hidden = true
+        yellowSelected.hidden = true
+        greenButton.hidden = true
+        greenSelected.hidden = true
+        blueButton.hidden = true
+        blueSelected.hidden = true
+        controls.hidden = true
+        frequencySlider.hidden = true
+        instructions.hidden = true
+        soundButton.hidden = true
 
     }
 
